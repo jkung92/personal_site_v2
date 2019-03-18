4920e009e6dd9f6b4d2940df014ac449e15d0fb3
Jonathan <jkung92@gmail.com>
2019-03-18 15:19:22 -0700

    added collapse to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'reactstrap';
+import { Navbar, Nav, NavItem, Collapse, NavbarToggler } from 'reactstrap';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-        <Navbar expand="md" sticky={'top'}>
-          <NavLink exact to="/" className="navbar-brand">
-            Jonathan Kung
-          </NavLink>
-
+      <Navbar dark expand="md" sticky={'top'}>
+        <NavLink exact to="/" className="navbar-brand">
+          Jonathan Kung
+        </NavLink>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="ml-auto mr-4" navbar>
             <NavItem>
               <NavLink to="/about">About</NavLink>
@@ -28,7 +43,8 @@ class NavBar extends Component {
               <NavLink to="/contact">Contact</NavLink>
             </NavItem>
           </Nav>
-        </Navbar>
+        </Collapse>
+      </Navbar>
     );
   }
 }
