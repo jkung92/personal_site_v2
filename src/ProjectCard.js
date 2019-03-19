6a0ec07e87257c3b3d53b4ae1f2bc18811944aa2
Jonathan <jkung92@gmail.com>
2019-03-18 17:13:39 -0700

    added proj details onClick

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,43 +1,128 @@
 import React, { Component } from 'react';
-import img1 from './static/01.png';
+// import img1 from './static/01.png';
 // import img2 from './static/02.png';
 // import img3 from './static/03.png';
 // import img5 from './static/05.png';
+import relish_screens from './static/relish.gif';
 import './ProjectCard.css';
 
 // Refactoring needed, pass down card info as props
+// TODO: Create a relish screenshot comp
 
 class ProjectCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+      isToggleOn2: true,
+      isToggleOn3: true,
+      isToggleOn4: true
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  };
+
+  handleClick2 = () => {
+    this.setState(prevState => ({
+      isToggleOn2: !prevState.isToggleOn2
+    }));
+  };
+
+  handleClick3 = () => {
+    this.setState(prevState => ({
+      isToggleOn3: !prevState.isToggleOn3
+    }));
+  };
+
+  handleClick4 = () => {
+    this.setState(prevState => ({
+      isToggleOn4: !prevState.isToggleOn4
+    }));
+  };
+
+  renderFirstCard = () => {
+    return (
+      <span>
+        <h2 style={{ color: 'lightsalmon' }}>PlayList Maker</h2>
+        <h5 style={{ color: '#61b8ff' }}>Express | Nunjucks</h5>
+        <span className="icon">
+          <a
+            href="https://github.com/jon-kung/spotify-web-api-app"
+            target={'_blank'}
+          >
+            <i className="fab fa-github mr-3" />
+          </a>
+          <a href="https://jons-playlist-maker.herokuapp.com" target={'_blank'}>
+            <i className="fas fa-external-link-alt mr-3" />
+          </a>
+        </span>
+      </span>
+    );
+  };
+
+  renderSecondCard = () => {
+    return (
+      <span>
+        <h2 style={{ color: 'indianred' }}>Jobly</h2>
+        <h5 style={{ color: '#61b8ff' }}>React | Express | Postgres</h5>
+        <span className="icon">
+          <a href="https://github.com/jon-kung/react-jobly" target={'_blank'}>
+            <i className="fab fa-github mr-3" />
+          </a>
+          <a href="https://jobly-frontend-jk.herokuapp.com/" target={'_blank'}>
+            <i className="fas fa-external-link-alt mr-3" />
+          </a>
+        </span>
+      </span>
+    );
+  };
+
+  renderThirdCard = () => {
+    return (
+      <span>
+        <h2 style={{ color: 'darksalmon' }}>The Relish CMS</h2>
+        <h5 style={{ color: '#61b8ff' }}>React | Redux | D3</h5>
+        <span className="icon">
+          <a href={relish_screens} target={'_blank'}>
+            <i className="fas fa-external-link-alt" />
+          </a>
+        </span>
+      </span>
+    );
+  };
+
+  renderFourthCard = () => {
+    return (
+      <span>
+        <h2 style={{ color: 'salmon' }}>HackerSnooze </h2>
+        <h5 style={{ color: '#61b8ff' }}>Javascript | HTML | CSS</h5>
+        <span className="icon">
+          <a
+            href="https://github.com/jon-kung/Hacker-News-Clone"
+            target={'_blank'}
+          >
+            <i className="fab fa-github" />
+          </a>
+        </span>
+      </span>
+    );
+  };
+
   render() {
     return (
       <div className="container mt-5">
         <div className="big-font" data-aos="fade-up" data-aos-duration="1000">
           Projects
         </div>
-        {/* <div class="card text-white" style={{
-              background: 'rgb(1, 1, 41)',
-              opacity: 0.9,
-              padding: '20px',
-              borderRadius: '8px',
-              boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'
-            }}>
-          <img
-            class="card-img"
-            src={img1}
-            alt="Card"
-          />
-          <div class="card-img-overlay">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p class="card-text">Last updated 3 mins ago</p>
-          </div>
-        </div> */}
+        <p data-aos="fade-up" data-aos-duration="2000">
+          {' '}
+          Click on each project for more details!
+        </p>
 
-        {/* End Testing */}
         <div className="row mt-5">
           <div
             data-aos="fade-up"
@@ -46,17 +131,18 @@ class ProjectCard extends Component {
             style={{
               opacity: 0.6
             }}
+            onClick={this.handleClick}
           >
-            <h2 style={{ color: 'lightsalmon' }}>PlayList Maker</h2>
-            <h5 style={{ color: '#61b8ff' }}>Express | Nunjucks</h5>
-            <span className="icon">
-              <a href="https://github.com/jon-kung/spotify-web-api-app">
-                <i className="fab fa-github mr-3" />
-              </a>
-              <a href="https://jons-playlist-maker.herokuapp.com">
-                <i className="fas fa-external-link-alt mr-3" />
-              </a>
-            </span>
+            {this.state.isToggleOn ? (
+              this.renderFirstCard()
+            ) : (
+              <p data-aos="fade-in" data-aos-duration="2500">
+                Playlist Maker is the first side-project I built on my own
+                during bootcamp! I love music and knew I wanted to use Spotify's
+                API for this project. Please checkout the GitHub link for more
+                details.
+              </p>
+            )}
           </div>
 
           <div
@@ -66,17 +152,17 @@ class ProjectCard extends Component {
             style={{
               opacity: 0.6
             }}
+            onClick={this.handleClick2}
           >
-            <h2 style={{ color: 'indianred' }}>Jobly</h2>
-            <h5 style={{ color: '#61b8ff' }}>React | Express | Postgres</h5>
-            <h6 className="icon">
-              <a href="https://github.com/jon-kung/react-jobly">
-                <i className="fab fa-github mr-3" />
-              </a>
-              <a href="https://jobly-frontend-jk.herokuapp.com/">
-                <i className="fas fa-external-link-alt mr-3" />
-              </a>
-            </h6>
+            {this.state.isToggleOn2 ? (
+              this.renderSecondCard()
+            ) : (
+              <p data-aos="fade-in" data-aos-duration="2500">
+                Jobly is a full-stack job application website I built during
+                bootcamp. Please use abc123 as both the username/password if
+                you'd like to check it out!
+              </p>
+            )}
           </div>
 
           <div
@@ -86,14 +172,16 @@ class ProjectCard extends Component {
             style={{
               opacity: 0.6
             }}
+            onClick={this.handleClick3}
           >
-            <h2 style={{ color: 'darksalmon' }}>Microblog</h2>
-            <h5 style={{ color: '#61b8ff' }}>React | Redux | Express</h5>
-            <h5 className="icon">
-              <a href="https://github.com/jon-kung/react-microblog">
-                <i className="fab fa-github" />
-              </a>
-            </h5>
+            {this.state.isToggleOn3 ? (
+              this.renderThirdCard()
+            ) : (
+              <p data-aos="fade-in" data-aos-duration="2500">
+                The Relish is a sports media start-up that I interned at. I was able to implement search
+                functionality, data visualization, and unit tests using Jest.
+              </p>
+            )}
           </div>
 
           <div
@@ -103,14 +191,16 @@ class ProjectCard extends Component {
             style={{
               opacity: 0.6
             }}
+            onClick={this.handleClick4}
           >
-            <h2 style={{ color: 'salmon' }}>HackerSnooze </h2>
-            <h5 style={{ color: '#61b8ff' }}>Javascript | HTML | CSS</h5>
-            <h6 className="icon">
-              <a href="https://github.com/jon-kung/Hacker-News-Clone">
-                <i className="fab fa-github" />
-              </a>
-            </h6>
+            {this.state.isToggleOn4 ? (
+              this.renderFourthCard()
+            ) : (
+              <p data-aos="fade-in" data-aos-duration="2500">
+                This site is a clone of hackernews.com, a full-stack app built
+                with Javascript, jQuery, and an OOP design.
+              </p>
+            )}
           </div>
         </div>
       </div>
