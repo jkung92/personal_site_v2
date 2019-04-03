f78d04bf946e3012d91702d8b21af5eb2ac94954
Jonathan <jkung92@gmail.com>
2019-04-03 15:37:13 -0700

    added resume

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import img4 from './static/04.jpg';
 import ContactIcons from './ContactIcons';
 import { NavLink } from 'react-router-dom';
+import resume from './static/Resume.pdf';
 
 class Home extends Component {
   render() {
     return (
-      <div className="mid container">
+      <div className="mid container mb-5">
         <div className="big-font" data-aos="fade-in" data-aos-duration="1500">
           {' '}
           Hi!{' '}
@@ -18,14 +19,17 @@ class Home extends Component {
           className="justify-content-center mt-5"
           data-aos="fade-up"
           data-aos-duration="3000"
-          style={{fontWeight: '540'}}
+          style={{ fontWeight: '540' }}
         >
-          <i class="fas fa-layer-group" /> Full-stack engineer that loves
-          working with APIs to build responsive,{' '}
-           user-friendly websites using modern
-          technologies like {' '}
+          <i class="fas fa-layer-group" /> Full-stack engineer that loves building
+          responsive, user-friendly websites using modern technologies like{' '}
           <i class="fab fa-react" /> React and <i class="fab fa-node-js" />{' '}
-          Node.js! Please check out my <NavLink to="/about">about</NavLink> and{' '}
+          Node.js! Please check out my <i class="fas fa-file" />{' '}
+          <a href={resume} target={'_blank'}>
+            {' '}
+            resume
+          </a>{' '}
+          or my <NavLink to="/about">about</NavLink> and{' '}
           <NavLink to="/projects">project</NavLink> page!
         </h5>
         <img
@@ -37,7 +41,7 @@ class Home extends Component {
           style={{
             height: '130px',
             width: '130px',
-            borderRadius: '50%',
+            borderRadius: '50%'
             // filter: 'grayscale(100%)'
           }}
         />
